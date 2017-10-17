76a187b7728e749b898e991a192ad62f0fa42fc9
Benjamin Hodgson <bhodgson@stackoverflow.com>
2017-10-17 20:33:05 +0100

    set up css theme

--- site.hs
+++ site.hs
@@ -53,7 +53,6 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
                     defaultContext
 
             getResourceBody
