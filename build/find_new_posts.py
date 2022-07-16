93aa839d66e7dbc1dbc0c204f119217ce7caa90e
Benjamin Hodgson <3857587+benjamin-hodgson@users.noreply.github.com>
2022-07-15 23:53:50 -0400

    exclusive date range

--- build/find_new_posts.py
+++ build/find_new_posts.py
@@ -5,7 +5,7 @@ import sys
 from datetime import date
 
 for f in os.listdir("posts"):
-    if date.fromisoformat(sys.argv[1][0:10]) <= date.fromisoformat(f[0:10]) <= date.today():
+    if date.fromisoformat(sys.argv[1][0:10]) < date.fromisoformat(f[0:10]) <= date.today():
         print("::set-output name=result::true")
         exit()
 print("::set-output name=result::false")
